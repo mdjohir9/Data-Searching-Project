1e0dbecb6eceb481eb18302e2c18d105cbb2aa6f
Md Johir Raihan <mdjohirullislam728@gmail.com>
2022-11-19 22:27:32 +0600

    Data Searching Project Coding Added Successfull

---
+++ Searching_project/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace Searching_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbl_save = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.text_first_name = new System.Windows.Forms.TextBox();
+            this.text_second_name = new System.Windows.Forms.TextBox();
+            this.text_id = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.update_btn = new System.Windows.Forms.Button();
+            this.Delate_btn = new System.Windows.Forms.Button();
+            this.Save_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.text_search_box = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cb_cloumn = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.data_grid_view = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.First_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Second_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.data_grid_view)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lbl_save);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.text_first_name);
+            this.groupBox1.Controls.Add(this.text_second_name);
+            this.groupBox1.Controls.Add(this.text_id);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.update_btn);
+            this.groupBox1.Controls.Add(this.Delate_btn);
+            this.groupBox1.Controls.Add(this.Save_btn);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(24, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 388);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oparetions";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(138, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Search";
+            // 
+            // lbl_save
+            // 
+            this.lbl_save.AutoSize = true;
+            this.lbl_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_save.ForeColor = System.Drawing.Color.White;
+            this.lbl_save.Location = new System.Drawing.Point(138, 233);
+            this.lbl_save.Name = "lbl_save";
+            this.lbl_save.Size = new System.Drawing.Size(0, 16);
+            this.lbl_save.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Secod Name :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID :";
+            // 
+            // text_first_name
+            // 
+            this.text_first_name.Location = new System.Drawing.Point(128, 130);
+            this.text_first_name.Multiline = true;
+            this.text_first_name.Name = "text_first_name";
+            this.text_first_name.Size = new System.Drawing.Size(161, 28);
+            this.text_first_name.TabIndex = 1;
+            // 
+            // text_second_name
+            // 
+            this.text_second_name.Location = new System.Drawing.Point(128, 184);
+            this.text_second_name.Multiline = true;
+            this.text_second_name.Name = "text_second_name";
+            this.text_second_name.Size = new System.Drawing.Size(161, 28);
+            this.text_second_name.TabIndex = 1;
+            // 
+            // text_id
+            // 
+            this.text_id.Location = new System.Drawing.Point(128, 86);
+            this.text_id.Multiline = true;
+            this.text_id.Name = "text_id";
+            this.text_id.Size = new System.Drawing.Size(94, 28);
+            this.text_id.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(228, 85);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 29);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "featch";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // update_btn
+            // 
+            this.update_btn.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.update_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update_btn.ForeColor = System.Drawing.Color.Black;
+            this.update_btn.Location = new System.Drawing.Point(24, 289);
+            this.update_btn.Name = "update_btn";
+            this.update_btn.Size = new System.Drawing.Size(78, 29);
+            this.update_btn.TabIndex = 0;
+            this.update_btn.Text = "Update";
+            this.update_btn.UseVisualStyleBackColor = false;
+            // 
+            // Delate_btn
+            // 
+            this.Delate_btn.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Delate_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delate_btn.ForeColor = System.Drawing.Color.Black;
+            this.Delate_btn.Location = new System.Drawing.Point(24, 339);
+            this.Delate_btn.Name = "Delate_btn";
+            this.Delate_btn.Size = new System.Drawing.Size(78, 29);
+            this.Delate_btn.TabIndex = 0;
+            this.Delate_btn.Text = "Delate";
+            this.Delate_btn.UseVisualStyleBackColor = false;
+            // 
+            // Save_btn
+            // 
+            this.Save_btn.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Save_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save_btn.ForeColor = System.Drawing.Color.Black;
+            this.Save_btn.Location = new System.Drawing.Point(24, 233);
+            this.Save_btn.Name = "Save_btn";
+            this.Save_btn.Size = new System.Drawing.Size(78, 29);
+            this.Save_btn.TabIndex = 0;
+            this.Save_btn.Text = "Save";
+            this.Save_btn.UseVisualStyleBackColor = false;
+            this.Save_btn.Click += new System.EventHandler(this.Save_btn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(15, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show Record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text_search_box
+            // 
+            this.text_search_box.Location = new System.Drawing.Point(173, 51);
+            this.text_search_box.Multiline = true;
+            this.text_search_box.Name = "text_search_box";
+            this.text_search_box.Size = new System.Drawing.Size(189, 28);
+            this.text_search_box.TabIndex = 1;
+            this.text_search_box.TextChanged += new System.EventHandler(this.text_search_box_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cb_cloumn);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.data_grid_view);
+            this.groupBox2.Controls.Add(this.text_search_box);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(363, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(479, 421);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // cb_cloumn
+            // 
+            this.cb_cloumn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_cloumn.FormattingEnabled = true;
+            this.cb_cloumn.Items.AddRange(new object[] {
+            "All",
+            "ID",
+            "FirstName",
+            "SecondName"});
+            this.cb_cloumn.Location = new System.Drawing.Point(6, 55);
+            this.cb_cloumn.Name = "cb_cloumn";
+            this.cb_cloumn.Size = new System.Drawing.Size(147, 24);
+            this.cb_cloumn.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(6, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Select Items";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(170, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Search";
+            // 
+            // data_grid_view
+            // 
+            this.data_grid_view.BackgroundColor = System.Drawing.SystemColors.Highlight;
+            this.data_grid_view.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_grid_view.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.First_name,
+            this.Second_name});
+            this.data_grid_view.GridColor = System.Drawing.Color.DodgerBlue;
+            this.data_grid_view.Location = new System.Drawing.Point(6, 114);
+            this.data_grid_view.Name = "data_grid_view";
+            this.data_grid_view.Size = new System.Drawing.Size(467, 301);
+            this.data_grid_view.TabIndex = 2;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // First_name
+            // 
+            this.First_name.HeaderText = "First Name :";
+            this.First_name.Name = "First_name";
+            // 
+            // Second_name
+            // 
+            this.Second_name.HeaderText = "Second Name";
+            this.Second_name.Name = "Second_name";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(368, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 29);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(878, 466);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.data_grid_view)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_first_name;
+        private System.Windows.Forms.TextBox text_second_name;
+        private System.Windows.Forms.TextBox text_id;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button update_btn;
+        private System.Windows.Forms.Button Delate_btn;
+        private System.Windows.Forms.Button Save_btn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text_search_box;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lbl_save;
+        private System.Windows.Forms.DataGridView data_grid_view;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn First_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Second_name;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cb_cloumn;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Searching_project/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Searching_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            cb_cloumn.SelectedIndex = 0;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            text_id.Text="";
+            text_first_name.Text = "";
+            text_second_name.Text = "";
+            text_search_box.Text = "";
+
+
+
+        }
+
+        private void Save_btn_Click(object sender, EventArgs e)
+        {
+
+            string Id = text_id.Text;
+            string First_name=text_first_name.Text;
+            string Second_name=text_second_name.Text;
+            
+
+            string connectionString = "Server=DESKTOP-JAHFGFD\\SQLEXPRESS;Database=student_info;Integrated Security=true";
+
+            string query = "INSERT INTO Search_engine Values('" + Id + "','" + First_name + "','" + Second_name + "')";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            int row_affected = cmd.ExecuteNonQuery();
+
+            if (row_affected > 0)
+            {
+                lbl_save.Text = "Saved Sucessfull";
+            }
+            else
+            {
+                lbl_save.Text = "Saved Field";
+            }
+
+            data_grid_view.Rows.Add(text_id.Text, text_first_name.Text, text_second_name.Text);
+
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+
+
+        //}
+
+
+
+        private void text_search_box_TextChanged(object sender, EventArgs e)
+        {
+
+            string connectionString = "Server=DESKTOP-JAHFGFD\\SQLEXPRESS;Database=student_info;Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            string Search_data = text_search_box.Text;
+            string Query = "SELECT * FROM Search_engine";
+
+            if (cb_cloumn.SelectedIndex ==0)
+            {
+                Query += " where First_name like'%" + Search_data + "%' or Second_name like'%" + Search_data + "%'";
+
+
+                if (int.TryParse(Search_data, out _))
+                {
+                    Query += " OR Id="+Search_data;
+                }
+
+            }
+            else
+            {
+                if (cb_cloumn.SelectedIndex == 1 && Search_data.Length>0)
+                {
+                    Query += " WHERE Id = "+Search_data;         
+                    
+                }
+
+                else if (cb_cloumn.SelectedIndex == 2)
+                {
+                    Query += " WHERE First_name LIKE '%" + Search_data+"%'";
+                }
+                else if(cb_cloumn.SelectedIndex == 3)
+                {
+                    Query += " WHERE Second_name LIKE '%" + Search_data + "%'";
+                }
+            }
+
+            SqlCommand cmd = new SqlCommand(Query, conn);
+
+            var reader = cmd.ExecuteReader();
+            data_grid_view.Rows.Clear();
+
+            while (reader.Read())
+            {
+                data_grid_view.Rows.Add(reader["Id"], reader["First_name"], reader["Second_name"]);
+            }
+            conn.Close();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string connectionString = "Server=DESKTOP-JAHFGFD\\SQLEXPRESS;Database=student_info;Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            string Search_data = text_search_box.Text;
+            string Query = "SELECT * FROM Search_engine where First_name like'%" + Search_data + "%' or Second_name like'%" + Search_data + "%'";
+            SqlCommand cmd = new SqlCommand(Query, conn);
+
+            var reader = cmd.ExecuteReader();
+            data_grid_view.Rows.Clear();
+
+            while (reader.Read())
+            {
+                data_grid_view.Rows.Add(reader["Id"], reader["First_name"], reader["Second_name"]);
+            }
+            conn.Close();
+        }
+    }
+}
